82ecb85ab17d2b91ee2c3652c317fe90578b3000
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-24 16:14:05 +0200

    Step_05

---
+++ Basics-01/Step_05.php
@@ -0,0 +1,14 @@
+<?php
+
+$course_name = " React JS";
+$enrolled_students = 5;
+$price = 20.50;
+$on_discount = false;
+
+// Course title: Rust, the language of no bugs
+// Enrolled Students: 20
+// Course price: 30 $ USD
+// Course on discount: Yes / No
+
+
+echo 'Course title: ' . $course_name . '\n' . 'Enrolled Students: ' . $enrolled_students . '\n' . 'Course price: ' . '\n' . 'Course on discount :' . $on_discount;
