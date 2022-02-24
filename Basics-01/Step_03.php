a54d31318af879b89162a1af767bb15401b56bf8
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-24 14:12:58 +0200

    Step-03

---
+++ Basics-01/Step_03.php
@@ -0,0 +1,12 @@
+<?php
+
+//Define
+$name = 'Khalid Agha';
+$email = 'mkhalid.k.agha@gmail.com';
+$height = 6;
+$width = 3;
+$eara = $width * $height;
+
+//Output
+echo "My name is $name and there is my email also $email \n";
+print "Height is  $height and  Width is $width so the eara is : $eara \n ";
