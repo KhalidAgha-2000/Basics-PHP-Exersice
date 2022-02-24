953c1cdfda3e517e98a89a6b2b1a14e4ddb38b04
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-24 17:03:44 +0200

    Step_08

---
+++ Basics-01/Step_08.php
@@ -0,0 +1,6 @@
+<?php
+
+$grocery = ["Eggs", "Milk", " Cheese", "Water", "Pack", "Tissues", " Watermelon"];
+
+$lasIndex = end($grocery);
+echo "Hello Sir, do you have $grocery[0], $grocery[1] and $grocery[2] ? Also if you sell fruits I find a $lasIndex  ";
