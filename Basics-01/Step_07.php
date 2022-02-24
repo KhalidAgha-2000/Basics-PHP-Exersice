a01ed9696019158513ac52781c0f33e2951eefc6
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-24 16:35:17 +0200

    Step_07

---
+++ Basics-01/Step_07.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$birth_year = 1990;
+$current_year = 2022;
+$different = $current_year - $birth_year;
+
+
+if ($different > 18) {
+    echo "You can drive \n";
+} else  echo "You still a kid, go and play GTA \n";
