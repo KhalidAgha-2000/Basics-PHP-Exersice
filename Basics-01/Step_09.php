590b82e2d21232194da74c2ff3d2856e0776f1e8
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-24 20:04:52 +0200

    Step_09

---
+++ Basics-01/Step_09.php
@@ -0,0 +1,9 @@
+<?php
+// The var_dump() function dumps information about one or more variables. The information holds type and value of the variable(s).
+
+
+$name = "khalid";
+$family = "Agha";
+
+var_dump($name);
+var_dump($family);
