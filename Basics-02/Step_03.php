7344e0947d7cfb2ae0fd43fcceabe0691620521c
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-24 21:19:41 +0200

    Step_03

---
+++ Basics-02/Step_03.php
@@ -0,0 +1,8 @@
+<?php
+//Should assign the variable to save it then we swappe it !!!!
+$var1 = 32;
+$var2 = 45;
+$var1_BeforeSwapping = $var1;
+
+echo $var1 = $var2 . "\n";
+echo $var2 = $var1_BeforeSwapping . "\n";
