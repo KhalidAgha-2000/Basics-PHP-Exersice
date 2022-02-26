f5acc4965f8192d4e04af0d2409f54cfc2f8e167
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-26 17:40:06 +0200

    Step_07

--- Basics-02/Step_07.php
+++ Basics-02/Step_07.php
@@ -1,28 +1,8 @@
 <?php
 
-/***************************************** */
-
-// $string1 = "php and python";
-// echo str_replace("php", "python", $string1);
-
 function replace($string)
 {
-
-    // $string = "you are learning php and then you have python";
     echo str_replace("php", "python", $string);
     echo str_replace("python", "php", $string);
 }
 replace("you are learning php and then you have python \n");
-// function sum($number)
-// {
-//     $number . '';
-//     $total = 0;
-//     for ($i = 0; $i < strlen($number); $i++) {
-//         $total += $total[$i];
-//     }
-// }
-// sum(2);
-// sum(2);
-// sum(2);
-// sum(3);
-// sum(5);
