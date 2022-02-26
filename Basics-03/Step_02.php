b52ed83c76579907ece61fa0581cd0cc73cf3daa
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-26 18:35:38 +0200

    Step_02

---
+++ Basics-03/Step_02.php
@@ -0,0 +1,13 @@
+<?php
+
+function stars($number)
+{
+    $i = 1;
+    $f = 1;
+    while ($i <= $number) {
+        $f = $f * $i;
+        $i++;
+    }
+    printf(" The Factorial of $number is $f, \n ");
+}
+stars(3);
