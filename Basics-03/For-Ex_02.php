78ff3847b3bb8982f82031d196ff632016d11dee
khalid <mkhalid.k.agha@gmail.comgit>
2022-02-26 21:24:57 +0200

    for Ex02

--- Basics-03/For-Ex_02.php
+++ Basics-03/For-Ex_02.php
@@ -6,11 +6,11 @@ for ($i = 1; $i <= $n; $i++) {
     for ($j = 1; $j <= $i; $j++) {
         echo ' * ';
     }
-    echo "\n";
+    echo PHP_EOL;
 }
 for ($i = $n; $i >= 1; $i--) {
     for ($j = 1; $j <= $i; $j++) {
         echo ' * ';
     }
-    echo "\n";
+    echo PHP_EOL;
 }
